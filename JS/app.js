dad39590f27b9f9441a6e17666f20dd0a879be02
morseck00 <morseck00@gmail.com>
2019-11-01 17:54:49 +0000

    1er commit

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+function add(n1, n2) {
+    alert(n1 + n2);
+}
+
+//Déclaration de mes variables en java Script
+var p1 = prompt('Votre 1er Nombre');
+var p2 = prompt('Votre 2em Nombre');
+add(number(p1) + number(p2));
+console.log();
